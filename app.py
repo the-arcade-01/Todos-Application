6157e7d0b4f5fd693e1782c9cfae43f3831f54af
aashisharcade@gmail.com <aashisharcade@gmail.com>
2019-11-17 07:18:05 +0530

    database created

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, session, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "hello"
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+class todos(db.Model):
+	_id = db.Column("id",db.Integer, primary_key=True)
+	data = db.Column(db.String(200))
+
+	def __init__(self,data):
+		self.data = data
+
+@app.route('/',methods=["POST","GET"])
 def index():
-	return render_template('base.html')
-
+	if request.method == "POST":
+		t = request.form["intodo"]
+		td = todos(t)
+		db.session.add(td)
+		db.session.commit()
+	
+	return render_template('index.html',values=todos.query.all())
+	
 if __name__=='__main__':
+	db.create_all()
 	app.run(debug=True)
